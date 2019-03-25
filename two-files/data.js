d6c0fc0f24010fe309ccea349d86fe3575857b37
huayifeng <461249104@qq.com>
2019-03-25 23:56:28 +0800

    feature: two files

---
+++ two-files/data.js
@@ -0,0 +1,31 @@
+export const plays = {
+  "hamlet": {
+    "name": "Hamlet",
+    "type": "tragedy"
+  },
+  "as-like": {
+    "name": "As You Like It",
+    "type": "comedy"
+  },
+  "othello": {
+    "name": "Othello",
+    "type": "tragedy"
+  }
+}
+
+export const invoice = {
+  "customer": "BigCo",
+  "performances": [{
+      "playID": "hamlet",
+      "audience": 55
+    },
+    {
+      "playID": "as-like",
+      "audience": 35
+    },
+    {
+      "playID": "othello",
+      "audience": 40
+    }
+  ]
+}
